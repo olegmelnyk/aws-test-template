72c4cdf3df76d8d120e5933da68fb4b0656b89c5
Oleh Melnyk <oleg902004@gmail.com>
2020-08-31 22:48:45 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### AWS CloudFormation Template
+
+[AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html) is required to be installed to run next commands.
+
+npm run template:stack:create - to create a stack
+npm run template:stack:delete - to delete a stack
---
+++ scripts/aws-template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+action=$1
+
+if [[ "$action" == "create" ]]; then
+  aws cloudformation create-stack --profile oleh-test \
+  --region eu-central-1 \
+  --stack-name fgactest \
+  --capabilities CAPABILITY_IAM \
+  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch \
+  --parameters ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
+  --template-body file://$PWD/template.yaml
+elif [[ "$action" == "update" ]]; then
+  aws cloudformation update-stack --profile oleh-test \
+  --region eu-central-1 \
+  --stack-name fgactest \
+  --capabilities CAPABILITY_IAM \
+  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch \
+  --parameters ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
+  --template-body file://$PWD/template.yaml
+elif [[ "$action" == "delete"  ]]; then
+  aws cloudformation delete-stack --profile oleh-test \
+  --region eu-central-1 \
+  --stack-name fgactest
+fi;
