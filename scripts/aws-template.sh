206a7e68a0622b70cace0ccfd4a941d1625f55e9
Oleh Melnyk <oleg902004@gmail.com>
2020-09-01 23:07:32 +0300

    update config params, remove typo, add kms keys for rest encryption

--- scripts/aws-template.sh
+++ scripts/aws-template.sh
@@ -7,16 +7,14 @@ if [[ "$action" == "create" ]]; then
   --region eu-central-1 \
   --stack-name fgactest \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch \
-  --parameters ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
+  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
   --template-body file://$PWD/template.yaml
 elif [[ "$action" == "update" ]]; then
   aws cloudformation update-stack --profile oleh-test \
   --region eu-central-1 \
   --stack-name fgactest \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch \
-  --parameters ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
+  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
   --template-body file://$PWD/template.yaml
 elif [[ "$action" == "delete"  ]]; then
   aws cloudformation delete-stack --profile oleh-test \
