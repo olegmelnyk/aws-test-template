2299ffc6513b2bb48554cf87a4391aa9541c22b3
Oleh Melnyk <oleg902004@gmail.com>
2020-09-07 22:32:46 +0300

    fix bugs, code rewrite to remove a callback hell

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+### Requirements
+
+It's required to create a [KMS key](https://eu-central-1.console.aws.amazon.com/kms/home) and specify the key id in lambda function (`keyId` variable).
+
 ### AWS CloudFormation Template
 
 [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html) is required to be installed to run next commands.
--- scripts/aws-template.sh
+++ scripts/aws-template.sh
@@ -2,22 +2,23 @@
 
 action=$1
 
+#--parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
+#--parameters ParameterKey=ZoneAwarenessEnabled,ParameterValue=true ParameterKey=DeploymentType,ParameterValue=production \
+
 if [[ "$action" == "create" ]]; then
-  aws cloudformation create-stack --profile oleh-test \
+  aws cloudformation create-stack --profile romexsoft-oleh \
   --region eu-central-1 \
   --stack-name fgactest \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
   --template-body file://$PWD/template.yaml
 elif [[ "$action" == "update" ]]; then
-  aws cloudformation update-stack --profile oleh-test \
+  aws cloudformation update-stack --profile romexsoft-oleh \
   --region eu-central-1 \
   --stack-name fgactest \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=MasterInstanceType,ParameterValue=t2.small.elasticsearch ParameterKey=DataInstanceType,ParameterValue=t2.small.elasticsearch \
   --template-body file://$PWD/template.yaml
 elif [[ "$action" == "delete"  ]]; then
-  aws cloudformation delete-stack --profile oleh-test \
+  aws cloudformation delete-stack --profile romexsoft-oleh \
   --region eu-central-1 \
   --stack-name fgactest
 fi;
